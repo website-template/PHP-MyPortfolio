6099222e41c55b44d88466a16c026f80469e04ad
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-06-30 22:25:34 +0300

    Create download_pdf_file.php

---
+++ file/download_pdf_file.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>İSMAİL TAŞDELEN | Download Pdf File Page</title>
+</head>
+<body>
+<?php
+header('Content-type: application/pdf');
+header('Content-Disposition: attachment; filename="cv_ismailtasdelen.pdf"');
+readfile('pdf/cv_ismailtasdelen.pdf');
+?>
+</body>
+</html> 
