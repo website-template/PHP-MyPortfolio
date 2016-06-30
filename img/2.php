346579452e5b86cb9e575b4b3adccf04b8d653a9
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-06-30 22:27:32 +0300

    Add files via upload

---
+++ img/2.php
@@ -0,0 +1,16 @@
+<HTML>
+	<HEAD>
+	<TITLE>404 Not Found</TITLE>
+	<BASE href="/error_docs/">
+	</HEAD>
+<BODY>
+	<H1>Not Found</H1>
+	<P>	
+	The requested document was not found on this server.
+	</P>
+	<HR>
+	<ADDRESS>
+	Web Server ismailtasdelen.com
+	</ADDRESS>
+</BODY>
+</HTML>
---
+++ img/3.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD>
+<TITLE>404 Not Found</TITLE>
+<BASE href="/error_docs/">
+</HEAD>
+<BODY>
+<H1>Not Found</H1>
+The requested document was not found on this server.
+<P>
+<HR>
+<ADDRESS>
+Web Server at ismailtasdelen.com
+</ADDRESS>
+</BODY>
+</HTML>
